93a670ee1461a6f27e936d4221b3db2488ad7d4b
reinis dreska <rdreska@gmail.com>
2021-11-23 10:56:28 +0200

    paam jatver

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-import webbrowser
+#Lai palaistu    py -m flask run
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Happy birthday, Latvia!"
-
-def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000/')
-
-if __name__ == "__main__":
-    open_browser();
-    app.run(port=5000)
